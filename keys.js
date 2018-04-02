89f06b8e690f85f79cb20fb7503c4840b5276cd0
Eliot Rodriguez <mr.ear84@gmail.com>
2018-04-02 13:06:47 -0400

    updating work adding dotenv code ready to create my api calls for spotify twitter and ombd

--- keys.js
+++ keys.js
@@ -1,12 +1,5 @@
 console.log("key.js is online");
 
-var TWITTER_CONSUMER_KEY = "eewJA7QIjugsfNog8zpBHHQk3";
-var TWITTER_CONSUMER_SECRET = "4fxzAeK9BBz7NSVvFROVWt2l42j8S1IkLQUNYbsAWqgsSt9lGZ";
-var TWITTER_ACCESS_TOKEN_KEY = "979388391598026752-Qm4T6BBmUfexPY5eYxhIm55XCvXLFAA";
-var TWITTER_ACCESS_TOKEN_SECRET = "	b7TyDDKHpJKvnxaFtIynuFavxk6krrz9eRXtAGnt0LAnc";
-var SPOTIFY_ID = "047ce9171e3f4232bbce8dab641327b7";
-var SPOTIFY_SECRET = "5931bd8c012a493facd86675445426da";
-
 exports.twitter = {
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
@@ -18,3 +11,11 @@ exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
 };
+
+/*
+module.exports = {
+  twitter: twitter,
+  spotify: spotify,
+  omdb: omdb
+};
+*/
--- liri.js
+++ liri.js
@@ -1,1 +1,9 @@
 require("dotenv").config();
+
+var keys = require("./keys.js");
+
+//var spotify = new Spotify(keys.spotify);
+//var client = new Twitter(keys.twitter);
+console.log(keys.spotify);
+//console.log("IN SPOTIFY: " + keys.spotify);
+//console.log("IN  CLIENT: " + keys.twitter);
