0aad74d79b203720e63dc2f8205a506668be0c01
Eliot Rodriguez <mr.ear84@gmail.com>
2018-04-02 14:06:29 -0400

    i am able to call to the twitter api now and am narrowing how to target tweets for a specific user

--- liri.js
+++ liri.js
@@ -1,9 +1,23 @@
 require("dotenv").config();
 
 var keys = require("./keys.js");
+var Twitter = require('twitter');
+
+var client = new Twitter(keys.twitter);
 
 //var spotify = new Spotify(keys.spotify);
 //var client = new Twitter(keys.twitter);
-console.log(keys.spotify);
+console.log("in client" + client);
 //console.log("IN SPOTIFY: " + keys.spotify);
 //console.log("IN  CLIENT: " + keys.twitter);
+
+var params = {screen_name: 'dummydev84', count: 20};
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
+  if (!error) {
+
+
+    console.log(tweets);
+
+
+  }
+});
