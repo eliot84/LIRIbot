8545d0ccf2b94692e8954803216b7799988fd8b7
Eliot Rodriguez <mr.ear84@gmail.com>
2018-04-03 15:27:04 -0400

    spacing in code fixed

--- liri.js
+++ liri.js
@@ -15,111 +15,102 @@ var ombdKey = keys.Ombd.key;
 
 var ombdCall = function(key, movie){
 
-if (movie == ""){
-    movie = 'Mr. Nobody';
-}
-  console.log('THE MOVE IS ' + movie);
-  request('http://www.omdbapi.com/?i=tt3896198&apikey=' + key + '&t=' + movie, function (error, response, body) {
-    console.log('error:', error); // Print the error if one occurred
-    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    if (movie == ""){
+        movie = 'Mr. Nobody';
+    }
+    console.log('THE MOVE IS ' + movie);
 
-    var result = JSON.parse(body);
+    request('http://www.omdbapi.com/?i=tt3896198&apikey=' + key + '&t=' + movie, function (error, response, body) {
+        console.log('error:', error); // Print the error if one occurred
+        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
 
-    //console.log(result.Title); // Print the HTML for the Google homepage.
-    console.log("########################");
-    console.log("MOVIES QUERY: ");
-    console.log("Title: " + result.Title);
-    console.log("Year: " + result.Year);
-    console.log("IMDB Rating: " + result.imdbRating);
-    console.log("Rotten Tomatoes Rating: " + result.Ratings[1].Value);
-    console.log("Country Produced: " + result.Country);
-    console.log("Language: " + result.Language);
-    console.log("Plot: " + result.Plot);
-    console.log("Plot: " + result.Actors);
-    console.log("########################");
-    console.log("");
-  });
+        var result = JSON.parse(body);
 
-}
+        //console.log(result.Title); // Print the HTML for the Google homepage.
+        console.log("########################");
+        console.log("MOVIES QUERY: ");
+        console.log("Title: " + result.Title);
+        console.log("Year: " + result.Year);
+        console.log("IMDB Rating: " + result.imdbRating);
+        console.log("Rotten Tomatoes Rating: " + result.Ratings[1].Value);
+        console.log("Country Produced: " + result.Country);
+        console.log("Language: " + result.Language);
+        console.log("Plot: " + result.Plot);
+        console.log("Plot: " + result.Actors);
+        console.log("########################");
+        console.log("");
+      });
+
+    }
 
 
 var spotifyCall = function(spotify, songname){
 
-  if (songname == ""){
+    if (songname == ""){
       songname = 'The Sign';
-  }
+    }
 
-  spotify.search({ type: 'track', query: songname, limit: 2 }, function(err, data) {
+    spotify.search({ type: 'track', query: songname, limit: 2 }, function(err, data) {
     if (err) {
       return console.log('Error occurred: ' + err);
     }
+
     console.log("########################");
     console.log("SPOTIFY SONG QUERY: ");
-    //Artist Name
     console.log("Artist: " + data.tracks.items[0].album.artists[0].name);
     console.log("");
-
-    //Song Name
     console.log("Song Name: " + data.tracks.items[0].name);
     console.log("");
-
-    //Preview URL
     console.log("Preview URL: " + data.tracks.items[0].preview_url);
     console.log("");
-
-    //Album Name
     console.log("Album Name: " + data.tracks.items[0].album.name);
     console.log("########################");
     console.log("");
-
   });
-
 }
 
 //call twitter api and check dummydev84 most recent 20 tweets
 var twitterCall = function(client){
-var params = {screen_name: 'dummydev84', count: 20};
-client.get('statuses/user_timeline', params, function(error, tweets, response) {
-  if (!error) {
-  // Print tweets returned
-    console.log("########################");
-    console.log("TWEETS FOR dummydev84: ");
-    console.log("");
-      for(var i = 0; i < tweets.length; i++){
-          console.log( (i + 1) + ". " + tweets[i].text);
-          console.log( twitterTime(tweets[i].created_at) );
-          console.log("");
-      } //end for
-     console.log("########################");
-     console.log("");
+    var params = {screen_name: 'dummydev84', count: 20};
 
-  } // end if
-}); //end client.get
-} //close twitterCall
-//twitterCall(client);
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+      if (!error) {
+      // Print tweets returned
+        console.log("########################");
+        console.log("TWEETS FOR dummydev84: ");
+        console.log("");
+          for(var i = 0; i < tweets.length; i++){
+              console.log( (i + 1) + ". " + tweets[i].text);
+              console.log( twitterTime(tweets[i].created_at) );
+              console.log("");
+          } //end for
+         console.log("########################");
+         console.log("");
+      }
+    });
+}
 
 
 //Takes the created timestamp at provided by twitter and reformats the time stamp to something legible.
 var twitterTime = function(timeStamp){
-  var newTime = moment(timeStamp, "ddd MMM D HH:mm:ss ZZ YYYY").format('MMMM D, YYYY h:mma');
-  return newTime;
+    var newTime = moment(timeStamp, "ddd MMM D HH:mm:ss ZZ YYYY").format('MMMM D, YYYY h:mma');
+    return newTime;
 }
 
 var doWhatItSaysCall = function(){
   //read from random.txt and perform the requested call with the QUERY
   //format for the file is call request type, QUERY
+    fs.readFile('random.txt', 'utf8', function (err, file) {
+        if (err) throw err;
+            var fromTheFile = file.split(',');
+            var query = fromTheFile[1];
 
-  fs.readFile('random.txt', 'utf8', function (err, file) {
-    if (err) throw err;
-    var fromTheFile = file.split(',');
-    var query = fromTheFile[1];
-
-    if(fromTheFile[0] == 'spotify-this-song'){
-      spotifyCall(spotify, query);
-    } else {
-      ombdCall(ombdKey, query);
-    }
-  });
+        if(fromTheFile[0] == 'spotify-this-song'){
+            spotifyCall(spotify, query);
+        } else {
+            ombdCall(ombdKey, query);
+        }
+    });
 }
 
 //add user prompt interaction info, without their permission not good :-(
